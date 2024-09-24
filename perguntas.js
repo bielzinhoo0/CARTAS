137152e07304431334ffd28c5ecc76004d6ef9a8
bielzinhoo0 <gabriel.moraes.santos01@escola.pr.gov.br>
2024-09-24 09:10:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o pais com mais título da copa do mundo?',
+    'Brasil'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual a sede da olímpiadas de 2024?',
+    'Paris'
+)
+
+criaCartao(
+    'Cidade',
+    'Qual a capital do paraná?',
+    'Curitiba'
+)
+
+criaCartao(
+    'Futsal',
+    'Qual o melhor ala destro 07 de sao mateus do sul ?',
+    'Bielzinhoo0_07'
+)
